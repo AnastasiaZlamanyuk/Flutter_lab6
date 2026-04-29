81aac4a3350ac7efc9c14396f30288932d996580
whypikmi <ttelatnikova1@yandex.ru>
2026-04-29 14:14:03 +0300

    Add coin protection, disabled button state and reset button

--- lib/slot_machine.dart
+++ lib/slot_machine.dart
@@ -11,6 +11,12 @@ class SlotMachine extends StatefulWidget {
 class _SlotMachineState
     extends State<SlotMachine> {
   void _spin() {
+    if (_coins <= 0) {
+      setState(() {
+        _message = 'Монеты закончились! 😥';
+      });
+      return;
+    }
     setState(() {
       _slot1 =
           _symbols[_random.nextInt(
@@ -32,6 +38,16 @@ class _SlotMachineState
         _message =
             'Попробуй ещё раз 😔 -1 монета';
       }
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _coins = 10;
+      _slot1 = 'assets/images/cherry.png';
+      _slot2 = 'assets/images/lemon.png';
+      _slot3 = 'assets/images/seven.png';
+      _message = '';
     });
   }
 
@@ -72,6 +88,7 @@ class _SlotMachineState
             ),
           ],
         ),
+
         SizedBox(height: 24),
         Text(
           _message,
@@ -82,7 +99,7 @@ class _SlotMachineState
         ),
         SizedBox(height: 40),
         ElevatedButton(
-          onPressed: _spin,
+          onPressed: _coins > 0 ? _spin : null,
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.amber,
             padding: EdgeInsets.symmetric(
@@ -96,6 +113,17 @@ class _SlotMachineState
               fontSize: 20,
               fontWeight: FontWeight.bold,
               color: Colors.black,
+            ),
+          ),
+        ),
+        SizedBox(height: 12),
+        TextButton(
+          onPressed: _reset,
+          child: Text(
+            'Начать заново',
+            style: TextStyle(
+              color: Colors.white70,
+              fontSize: 16,
             ),
           ),
         ),
